73776bdc37a78fcafcad2e2b58ffc357052d7c27
Yury Spektor <yspektor78@gmail.com>
2019-11-22 22:20:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,10 @@ def using_flatten(array)
   array.flatten
 end
 
+def using_delete(array,string)
+  array.delete(string)
+end
+
 def using_delete_at(array,integer)
   array.delete_at(integer)
 end
